0556b6936010eab10d3a9e6b0aef83ec34d3e3c0
Ahmed <ahmed.msku@gmail.com>
2025-11-22 14:52:57 -0800

    Section 3

---
+++ Core/Entities/IProductRepository.cs
@@ -0,0 +1,14 @@
+namespace Core.Entities;
+
+public interface IProductRepository
+{
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
+    Task<Product?> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<string>> GetBrandAsync();
+    Task<IReadOnlyList<string>> GetTypesAsync();
+    void AddProduct(Product product);
+    void UpdateProduct(Product product);
+    void DeleteProduct(Product product);
+    bool ProductExist(int id);
+    Task<bool> SaveChangesAsync();
+}
